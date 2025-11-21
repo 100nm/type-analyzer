cb352a097ecd3af24626a64bfe3cd4f243ea8718
remimd <remimd@github.com>
2025-11-21 22:07:48 +0100

    Project basis 🚀

---
+++ tests/core/test_matching_types.py
@@ -0,0 +1,84 @@
+from collections.abc import Callable
+from types import NoneType
+from typing import Annotated, Optional
+
+import pytest
+
+from type_analyzer import MatchingTypesConfig, matching_types
+
+
+def test_matching_types_with_annotated() -> None:
+    assert matching_types(Annotated[str, "metadata"]) == (str,)
+
+
+def test_matching_types_with_union() -> None:
+    assert matching_types(str | int) == (str, int)
+
+
+def test_matching_types_with_optional() -> None:
+    assert matching_types(str | None) == (str, NoneType)
+    assert matching_types(Optional[str]) == (str, NoneType)
+
+
+def test_matching_types_with_optional_and_ignore_none() -> None:
+    config = MatchingTypesConfig(ignore_none=True)
+    assert matching_types(str | None, config) == (str,)
+    assert matching_types(Optional[str], config) == (str,)
+
+
+def test_matching_types_with_class() -> None:
+    class Class: ...
+
+    config = MatchingTypesConfig(with_mro=True)
+    assert matching_types(Class, config) == (Class, object)
+
+
+def test_matching_types_with_generic_class() -> None:
+    class A[T]: ...
+
+    class B[T]: ...
+
+    class C[T1, T2](A[T1], B[T2]): ...
+
+    config = MatchingTypesConfig(with_mro=True)
+    assert matching_types(C[str, int], config) == (C[str, int], A[str], B[int])
+
+
+def test_matching_types_with_origin() -> None:
+    config = MatchingTypesConfig(with_origin=True)
+    assert matching_types(list[str], config) == (list[str], list)
+
+
+def test_matching_types_with_type_alias() -> None:
+    type String = str
+    config = MatchingTypesConfig(with_type_alias_value=True)
+    assert matching_types(String, config) == (String, str)
+
+
+def test_matching_types_with_generic_type_alias() -> None:
+    type StringOr[T] = str | T
+    config = MatchingTypesConfig(with_type_alias_value=True)
+    assert matching_types(StringOr[int], config) == (StringOr[int], str, int)
+
+
+@pytest.mark.skip("Need to associate a variable number of generic parameters")
+def test_matching_types_with_type_var_tuple() -> None:
+    type Tuple[*Ts] = tuple[*Ts]
+    config = MatchingTypesConfig(with_type_alias_value=True)
+    assert matching_types(Tuple[str, int], config) == (Tuple[str, int], tuple[str, int])
+
+
+@pytest.mark.skip("ParamSpec value isn't hashable")
+def test_matching_types_with_paramspec() -> None:
+    type Callback[**P] = Callable[P, None]
+    config = MatchingTypesConfig(with_type_alias_value=True)
+    assert matching_types(Callback[[str, int]], config) == (
+        Callback[[str, int]],
+        Callable[[str, int], None],
+    )
+
+
+def test_matching_types_with_mro_and_type_alias() -> None:
+    type String = str
+    config = MatchingTypesConfig(with_mro=True, with_type_alias_value=True)
+    assert matching_types(String, config) == (String, str, object)
---
+++ type_analyzer/_core/matching_types.py
@@ -0,0 +1,119 @@
+from collections.abc import Iterator, Mapping, Sequence
+from dataclasses import dataclass, field
+from types import NoneType, UnionType, get_original_bases
+from typing import (
+    Annotated,
+    Any,
+    Generic,
+    TypeAliasType,
+    TypeIs,
+    TypeVar,
+    Union,
+    get_args,
+    get_origin,
+)
+
+
+@dataclass(repr=False, eq=False, frozen=True, kw_only=True, slots=True)
+class MatchingTypesConfig:
+    ignore_none: bool = field(default=False)
+    with_mro: bool = field(default=False)
+    with_origin: bool = field(default=False)
+    with_type_alias_value: bool = field(default=False)
+
+
+def matching_types(
+    type_hint: Any,
+    /,
+    config: MatchingTypesConfig | None = None,
+) -> tuple[Any, ...]:
+    type_hints = (type_hint,)
+    return tuple(dict.fromkeys(_iter_matching_types(type_hints, config)))
+
+
+def _get_bases(type_hint: Any, origin: Any) -> tuple[Any, ...]:
+    target = type_hint if origin is None else origin
+
+    try:
+        return get_original_bases(target)
+    except TypeError:
+        return ()
+
+
+def _get_generics(type_hint: Any, origin: Any) -> Mapping[TypeVar, Any]:
+    args = get_args(type_hint)
+    parameters = getattr(origin, "__parameters__", ())
+    return dict(zip(parameters, args))
+
+
+def _is_type_var(type_hint: Any) -> TypeIs[TypeVar]:
+    return isinstance(type_hint, TypeVar)
+
+
+def _is_none(type_hint: Any) -> bool:
+    return type_hint in (None, NoneType)
+
+
+def _iter_matching_types(
+    type_hints: Sequence[Any],
+    /,
+    config: MatchingTypesConfig | None = None,
+    params: Mapping[TypeVar, Any] | None = None,
+) -> Iterator[Any]:
+    config = config or MatchingTypesConfig()
+
+    if params is None:
+        params = {}
+
+    for type_hint in type_hints:
+        if config.ignore_none and _is_none(type_hint):
+            continue
+
+        if _is_type_var(type_hint):
+            yield params[type_hint]
+            continue
+
+        origin = get_origin(type_hint)
+
+        if origin is Generic:
+            continue
+
+        if unpacked_types := _unpack_types(type_hint, origin):
+            yield from _iter_matching_types(unpacked_types, config, params)
+            continue
+
+        if origin is None:
+            yield type_hint
+
+        else:
+            iter_args = (
+                params[arg] if _is_type_var(arg) else arg for arg in get_args(type_hint)
+            )
+            yield origin[*iter_args]
+
+            if config.with_origin:
+                yield origin
+
+        if config.with_mro and (bases := _get_bases(type_hint, origin)):
+            generics = _get_generics(type_hint, origin)
+            yield from _iter_matching_types(bases, config, generics)
+
+        if config.with_type_alias_value:
+            if isinstance(origin, TypeAliasType):
+                values = (origin.__value__,)
+                generics = _get_generics(type_hint, origin)
+                yield from _iter_matching_types(values, config, generics)
+
+            elif isinstance(type_hint, TypeAliasType):
+                values = (type_hint.__value__,)
+                yield from _iter_matching_types(values, config)
+
+
+def _unpack_types(type_hint: Any, origin: Any) -> Sequence[Any]:
+    if origin is Union or isinstance(type_hint, UnionType):
+        return get_args(type_hint)
+
+    elif origin is Annotated:
+        return get_args(type_hint)[:1]
+
+    return ()
